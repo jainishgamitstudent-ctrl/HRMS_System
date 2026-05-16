1a84ccf72c53bb4f292e383ad8a69e24437fddc2
mishra-riya06 <mriya0619@gmail.com>
2026-05-16 16:17:00 +0530

    stagechnge

--- infiApBackend/src/models/holiday.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const holidaySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    date: { type: Date, required: true },
-    type: { type: String, enum: ["Public", "Company"], default: "Company" }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Holiday", holidaySchema);
